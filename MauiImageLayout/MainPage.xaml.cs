0c3be7649b9596c69f51e5c98a6e6b567ac8a46f
Timothy Miller <innerlogic4321@gmail.com>
2023-03-06 12:43:47 +0900

    Image Layout

---
+++ MauiImageLayout/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiImageLayout;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+        this.BindingContext = this;
+
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+        this.ImageLinks.Add("https://placehold.jp/150x150.png");
+    }
+    public ObservableCollection<string> ImageLinks { get; private set; } = new ObservableCollection<string>();
+}
+
