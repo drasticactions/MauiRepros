bf4610b4ec3401d4a81340779912d0dc82b181d1
Timothy Miller <innerlogic4321@gmail.com>
2023-03-16 19:01:08 +0900

    Add Webview Catalyst test

---
+++ WebviewTestCatalyst/AppDelegate.cs
@@ -0,0 +1,30 @@
+namespace WebviewTestCatalyst;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		// create a new window instance based on the screen size
+		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+		// create a UIViewController with a single UILabel
+		var vc = new UIViewController ();
+		var webview = new WebKit.WKWebView(Window!.Frame, new WebKit.WKWebViewConfiguration())
+		{
+            AutoresizingMask = UIViewAutoresizing.All
+        };
+		vc.View!.AddSubview (webview);
+		Window.RootViewController = vc;
+
+		// make the window visible
+		Window.MakeKeyAndVisible ();
+
+		webview.LoadRequest(new NSUrlRequest(new NSUrl("https://www.toptal.com/developers/keycode")));
+		return true;
+	}
+}
