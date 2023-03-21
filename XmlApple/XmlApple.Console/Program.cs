a002cb1308fa0925bdff2add4da375ad4f03b7ce
Timothy Miller <innerlogic4321@gmail.com>
2023-03-22 00:42:28 +0900

    Test console

---
+++ XmlApple/XmlApple.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(Test));
+
+Console.WriteLine("Hello, World!");
+
+
+
+public class Test
+{
+}
