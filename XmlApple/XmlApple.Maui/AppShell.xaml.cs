e801e9d85290011da839e363acbef1d4af300d59
Timothy Miller <innerlogic4321@gmail.com>
2023-03-22 00:36:14 +0900

    XML

---
+++ XmlApple/XmlApple.Maui/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace XmlApple.Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(Test));
+    }
+}
+
+public class Test
+{
+}
---
+++ XmlApple/XmlApple.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace XmlApple.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
