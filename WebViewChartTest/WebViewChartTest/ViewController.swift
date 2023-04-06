1b0e78cd81ce8165d37183a80f095269a48c4d68
Timothy Miller <innerlogic4321@gmail.com>
2023-04-06 17:47:46 +0900

    Hack a console in there

--- WebViewChartTest/WebViewChartTest/ViewController.swift
+++ WebViewChartTest/WebViewChartTest/ViewController.swift
@@ -18,6 +18,45 @@ class ViewController: UIViewController {
 #if DEBUG
         configuration.preferences.setValue(true, forKey: "developerExtrasEnabled")
 #endif
+        
+        // https://stackoverflow.com/questions/37159648/how-to-read-console-logs-of-wkwebview-programmatically
+        // We should not need this, but I can't get the Safari tools running in Catalyst.
+        let overrideConsole = """
+            function log(emoji, type, args) {
+              window.webkit.messageHandlers.logging.postMessage(
+                `${emoji} JS ${type}: ${Object.values(args)
+                  .map(v => typeof(v) === "undefined" ? "undefined" : typeof(v) === "object" ? JSON.stringify(v) : v.toString())
+                  .map(v => v.substring(0, 3000)) // Limit msg to 3000 chars
+                  .join(", ")}`
+              )
+            }
+
+            let originalLog = console.log
+            let originalWarn = console.warn
+            let originalError = console.error
+            let originalDebug = console.debug
+
+            console.log = function() { log("📗", "log", arguments); originalLog.apply(null, arguments) }
+            console.warn = function() { log("📙", "warning", arguments); originalWarn.apply(null, arguments) }
+            console.error = function() { log("📕", "error", arguments); originalError.apply(null, arguments) }
+            console.debug = function() { log("📘", "debug", arguments); originalDebug.apply(null, arguments) }
+
+            window.addEventListener("error", function(e) {
+               log("💥", "Uncaught", [`${e.message} at ${e.filename}:${e.lineno}:${e.colno}`])
+            })
+        """
+        
+        class LoggingMessageHandler: NSObject, WKScriptMessageHandler {
+            func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+                print(message.body)
+            }
+        }
+        
+        let userContentController = WKUserContentController()
+        userContentController.add(LoggingMessageHandler(), name: "logging")
+        userContentController.addUserScript(WKUserScript(source: overrideConsole, injectionTime: .atDocumentStart, forMainFrameOnly: true))
+        configuration.userContentController = userContentController
+        
         webView = WKWebView(frame: self.view.bounds, configuration: configuration)
                
         webView.translatesAutoresizingMaskIntoConstraints = false // disable autoresizing mask
@@ -40,7 +79,7 @@ class ViewController: UIViewController {
                     button.trailingAnchor.constraint(equalTo: view.trailingAnchor),
                     button.heightAnchor.constraint(equalToConstant: 50)
                 ])
-                       
+    
         // Load the web page
         let url = URL(string: "https://digital.fidelity.com/stgw/digital/fidchart/consumers/atp/0.0.4/atp-chart.html")!
         let request = URLRequest(url: url)
