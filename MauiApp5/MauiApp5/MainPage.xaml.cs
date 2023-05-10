e78951fa56c7e6f46339de2b1696e88fdffc3654
Timothy Miller <innerlogic4321@gmail.com>
2023-05-10 10:53:29 +0900

    Add Image

---
+++ MauiApp5/MauiApp5/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace MauiApp5;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		if (this.CounterBtn.ImageSource == null)
+		{
+			this.CounterBtn.ImageSource = "dotnet_bot.png";
+		}
+		else
+		{
+			this.CounterBtn.ImageSource = null;
+		}
+
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
