933b501496bbcc552161133b1d3b1d36b5a3baa0
Tim Miller <innerlogic4321@gmail.com>
2023-06-19 11:22:23 +0900

    Sql

---
+++ SqlSample/SqlSample/SampleDemoDatabase.cs
@@ -0,0 +1,42 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SqlSample
+{
+    public class SampleDemoDatabase
+    {
+        static object locker = new object();
+        SQLiteConnection database;
+        public SampleDemoDatabase(string appDirectory = "")
+        {
+            string dbPath = System.IO.Path.Combine(appDirectory, "mydatabase3.db");
+            SQLiteConnection connection = new SQLiteConnection(dbPath);
+            database = connection;
+            // Creating the table
+            database.CreateTable<OrderItem>();
+
+            // Inserting items into table
+            database.Query<OrderItem>("INSERT INTO OrderItem(BillDate) values('2023-06-12')");
+
+        }
+
+        public IEnumerable<OrderItem> GetItems()
+        {
+            lock (locker)
+            {
+                var table = from i in database.Table<OrderItem>() select i;
+                return table;
+            }
+        }
+
+    }
+
+    public class OrderItem
+    {
+        public DateTime BillDate { get; set; }
+
+    }
+}
---
+++ SqlSample/SqlSampleConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SqlSample;
+
+Console.WriteLine("Hello, World!");
+
+var database = new SampleDemoDatabase();
+var items = database.GetItems();
+foreach(var item in items)
+{
+    Console.WriteLine(item.BillDate);
+}
