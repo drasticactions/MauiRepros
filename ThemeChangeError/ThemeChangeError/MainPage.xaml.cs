a9442c16b1d93dc999d13892d008ce015b6bb112
Tim Miller <drasticactions@gmail.com>
2023-06-19 16:37:07 +0900

    Update

--- ThemeChangeError/ThemeChangeError/MainPage.xaml.cs
+++ ThemeChangeError/ThemeChangeError/MainPage.xaml.cs
@@ -36,14 +36,7 @@ public partial class MainPage : ContentPage
 
     private void OnCounterClicked(object sender, EventArgs e)
     {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
+       this.UpdateLabels();
     }
 
     private void Button_Clicked(object sender, EventArgs e)
