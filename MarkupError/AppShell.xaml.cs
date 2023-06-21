f16845e6d44f77f6fc2a5df6ae502fad9cf9cfe1
Tim Miller <innerlogic4321@gmail.com>
2023-06-22 00:18:56 +0900

    MarkupError done right

---
+++ MarkupError/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MarkupError;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MarkupError/StringResourceXamlMarkupExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkupError
+{
+    public class StringResourceXamlMarkupExtension : IMarkupExtension<string>
+    {
+        private string test;
+
+        public string Test {
+            get => test;
+            set => test = value;
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (this as IMarkupExtension<string>).ProvideValue(serviceProvider);
+        }
+
+        public string ProvideValue(IServiceProvider serviceProvider)
+        {
+            return "test";
+        }
+    }
+}
