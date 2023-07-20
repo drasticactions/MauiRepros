8c37c53de0b99ab1889ac559e191d4e989b3507f
Tim Miller <innerlogic4321@gmail.com>
2023-07-20 14:00:05 +0900

    Android

---
+++ OverflowAndroid/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace OverflowAndroid;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override void OnHandlerChanged()
+    {
+        base.OnHandlerChanged();
+		#if ANDROID
+		var blazorView = this.blazorWebView;
+		var platformView = (Android.Webkit.WebView)blazorView.Handler.PlatformView;
+		platformView.OverScrollMode = Android.Views.OverScrollMode.Never;
+		#endif
+    }
+}
