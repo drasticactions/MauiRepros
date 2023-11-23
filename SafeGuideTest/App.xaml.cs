c9b39460e84b3500d723e785f58c03f49e614196
Tim Miller <innerlogic4321@gmail.com>
2023-11-23 12:53:11 +0900

    Update

---
+++ SafeGuideTest/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SafeGuideTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ SafeGuideTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SafeGuideTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
