4151bc89405cd8482c09d45ddc7a67739f1bc39f
Tim Miller <innerlogic4321@gmail.com>
2023-12-29 11:37:26 +0900

    Theme bug

---
+++ MauiTheme/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiTheme;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		Application.Current.RequestedThemeChanged += (s, a) =>
+		{
+			this.Label.Text = $"RequestedThemeChanged: {a.RequestedTheme}";
+			System.Diagnostics.Debug.WriteLine($"RequestedThemeChanged: {a.RequestedTheme}");
+		};
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		this.DisplayAlert("Title", $"You clicked {++count} times", "OK");
+	}
+	
+}
+
