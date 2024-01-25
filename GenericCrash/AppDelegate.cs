c41c7aac7dfefab24cef89f6f3770eb130190914
Timothy Miller <innerlogic4321@gmail.com>
2024-01-26 00:03:36 +0900

    Generic Crash

---
+++ GenericCrash/AppDelegate.cs
@@ -0,0 +1,71 @@
+namespace GenericCrash;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		// create a new window instance based on the screen size
+		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+		InitializeList();
+
+		// create a UIViewController with a single UILabel
+		var vc = new UIViewController ();
+		var button = new UIButton (Window!.Frame) {
+			BackgroundColor = UIColor.SystemBackground,
+			AutoresizingMask = UIViewAutoresizing.All,
+		};
+		button.SetTitle ("Click me for average!", UIControlState.Normal);
+		button.TouchUpInside += (sender, e) => {
+			var average = ListItems.Average (x => x.Cost);
+			var alert = UIAlertController.Create ("Average", $"The average is {average}", UIAlertControllerStyle.Alert);
+			alert.AddAction (UIAlertAction.Create ("OK", UIAlertActionStyle.Default, null));
+			vc.PresentViewController (alert, true, null);
+		};
+		vc.View!.AddSubview (button);
+		Window.RootViewController = vc;
+
+		// make the window visible
+		Window.MakeKeyAndVisible ();
+
+		return true;
+	}
+
+	 private void InitializeList()
+        {
+            List<CustomModel> models = new List<CustomModel>();
+
+            for (int i = 0; i < 50000; i++)
+            {
+                models.Add(new CustomModel
+                {
+                    Title = "Test " + i,
+                    Cost = i
+                });
+            }
+            ListItems = models;
+        }
+
+        private List<CustomModel> listItems = [];
+
+        public List<CustomModel> ListItems
+        {
+            get { return listItems; }
+            set
+            {
+                listItems = value;
+            }
+        }
+}
+
+
+
+    public class CustomModel
+    {
+        public string Title { get; set; }
+        public decimal Cost { get; set; }
+    }
