5e43092044c6cd7de501bf37e2809efb02a7b447
Tim Miller <innerlogic4321@gmail.com>
2024-01-26 18:18:47 +0900

    App

---
+++ ListVirtualizationTest/ListVirtualizationTestCore/BasicLargeListModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ListVirtualizationTestCore;
+
+public class BasicLargeListModel
+{
+    public List<string> Items { get; }
+
+    public BasicLargeListModel(int count = 10000) {
+        Items = new List<string>();
+        for (int i = 0; i < count; i++)
+        {
+            this.Items.Add(i.ToString());
+        }
+    }
+}
