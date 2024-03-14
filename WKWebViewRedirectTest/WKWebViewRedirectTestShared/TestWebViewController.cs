8d2e5da11847213463d2a7157fa48e272b34af58
Tim Miller <innerlogic4321@gmail.com>
2024-03-14 15:34:09 +0900

    Update

--- WKWebViewRedirectTest/WKWebViewRedirectTestShared/TestWebViewController.cs
+++ WKWebViewRedirectTest/WKWebViewRedirectTestShared/TestWebViewController.cs
@@ -15,19 +15,33 @@ public class TestWebViewController : UIViewController
         this.View!.Add(webviewHolder);
 
         var configuration = new WebKit.WKWebViewConfiguration();
+        configuration.DefaultWebpagePreferences.AllowsContentJavaScript = true;
         // configuration.Preferences.SetValueForKey(NSNumber.FromBoolean(true), new NSString("developerExtrasEnabled"));
         this.webviewHolder.MakeConstraints(make => {
             make.Top.And.Left.And.Right.And.Bottom.EqualTo(this.View!);
             //make.Bottom.EqualTo(this.View).With.Offset(-200);
         });
 
-        webview = new WebKit.WKWebView(this.webviewHolder.Bounds, configuration);
+        webview = new CustomWebView(this.webviewHolder.Bounds, configuration);
+        webview.Inspectable = true;
         webview.NavigationDelegate = new CustomNavigationDelegate();
         this.webviewHolder.Add(webview);
 
         webview.MakeConstraints(make => make.Top.And.Bottom.And.Left.And.Right.EqualTo(this.webviewHolder!));
-        webview.LoadRequest(new NSUrlRequest(new NSUrl("https://archiverse.guide")));
+        webview.LoadRequest(new NSUrlRequest(new NSUrl("https://drasticactions.github.io/test.html")));
 
+    }
+    
+    public class CustomWebView : WKWebView
+    {
+        public CustomWebView(CGRect url, WKWebViewConfiguration configuration) : base(url, configuration)
+        {
+        }
+
+        public override void BuildMenu(IUIMenuBuilder builder)
+        {
+            base.BuildMenu(builder);
+        }
     }
     
     public class CustomNavigationDelegate : WKNavigationDelegate
