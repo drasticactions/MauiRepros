3a735e3f3db2ebf2b63c604473203f9ebb3629e6
Tim Miller <innerlogic4321@gmail.com>
2024-04-08 16:31:58 +0900

    Tap

---
+++ AndroidTapThroughIssue/AndroidTapThroughIssue/AndroidTapThroughIssue/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AndroidTapThroughIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _count;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            _count++;
+
+            CounterBtn.Text = _count == 1 ? $"Clicked {_count} time" : $"Clicked {_count} times";
+
+        }
+    }
+}
