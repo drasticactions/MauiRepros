9517bb6ba9085c888226fdc554d7cf48a5ee8b40
Tim Miller <innerlogic4321@gmail.com>
2024-04-16 16:42:38 +0900

    Update

---
+++ MauiThrowTest/MauiThrowTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiThrowTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiThrowTest/MauiThrowTest/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace MauiThrowTest
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            if (!this.Working)
+            {
+                new Thread(this.DoWork).Start();
+            }
+
+            this.Working = true;
+
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private bool Working = false;
+
+        private void DoWork()
+        {
+            while (true)
+            {
+                try
+                {
+                    throw new Exception("This is a test exception");
+                }
+                catch (Exception e)
+                {
+                }
+            }
+        }
+    }
+
+}
